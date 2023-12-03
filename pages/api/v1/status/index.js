8416a629fd5c01fcba30498f3eead83bf8bf39df
Rafael Tapia <rafaelsilvatapia@gmail.com>
2023-12-03 12:36:03 -0300

    implement status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,25 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("Select 1 + 1 as sum;");
+  const pgVersionResult = await database.query(`SHOW SERVER_VERSION;`);
+  const maxConnectionsResult = await database.query(`SHOW MAX_CONNECTIONS;`);
+  const connectionsResult = await database.query(
+    `SELECT count(1) FROM pg_stat_activity;`,
+  );
 
-  console.log({ result: result.rows });
+  const updatedAt = new Date().toISOString();
+  const pgVersion = pgVersionResult.rows[0].server_version;
+  const maxConnections = Number(maxConnectionsResult.rows[0].max_connections);
+  const usedConnections = Number(connectionsResult.rows[0].count);
 
-  response.status(200).json({ success: true });
+  response.status(200).json({
+    updated_at: updatedAt,
+    database: {
+      version: pgVersion,
+      max_connections: maxConnections,
+      used_connections: usedConnections,
+    },
+  });
 }
 
 export default status;
