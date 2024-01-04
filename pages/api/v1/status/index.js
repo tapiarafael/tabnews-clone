0de00bfe93758d0cb77fe5e0e0510a13d5b2a56d
Rafael Tapia <rafaelsilvatapia@gmail.com>
2024-01-04 08:50:47 -0300

    finish /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,23 +1,27 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const pgVersionResult = await database.query(`SHOW SERVER_VERSION;`);
-  const maxConnectionsResult = await database.query(`SHOW MAX_CONNECTIONS;`);
-  const connectionsResult = await database.query(
-    `SELECT count(1) FROM pg_stat_activity;`,
-  );
+  const pgVersionResult = await database.query(`SHOW server_version;`);
+  const maxConnectionsResult = await database.query(`SHOW max_connections;`);
+  const dbName = process.env.POSTGRES_DB;
+  const openedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
 
   const updatedAt = new Date().toISOString();
   const pgVersion = pgVersionResult.rows[0].server_version;
   const maxConnections = Number(maxConnectionsResult.rows[0].max_connections);
-  const usedConnections = Number(connectionsResult.rows[0].count);
+  const openedConnections = openedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
-    database: {
-      version: pgVersion,
-      max_connections: maxConnections,
-      used_connections: usedConnections,
+    dependencies: {
+      database: {
+        version: pgVersion,
+        max_connections: maxConnections,
+        opened_connections: openedConnections,
+      },
     },
   });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,17 +3,11 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
 
   const parsedDate = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedDate);
 
-  expect(responseBody.database.version).toBeDefined();
-  expect(typeof responseBody.database.version).toEqual("string");
-
-  expect(responseBody.database.max_connections).toBeDefined();
-  expect(typeof responseBody.database.max_connections).toEqual("number");
-
-  expect(responseBody.database.used_connections).toBeDefined();
-  expect(typeof responseBody.database.used_connections).toEqual("number");
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
