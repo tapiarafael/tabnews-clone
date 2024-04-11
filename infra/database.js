420f13f00e02fbbcd5c2fe086008625c78561f52
Rafael Tapia <rafaeltapia@MacBook-Pro-de-Rafael.local>
2024-04-11 15:06:45 -0300

    fix: put db.connect inside a trycatch

--- infra/database.js
+++ infra/database.js
@@ -8,13 +8,14 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
   });
-  await client.connect();
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.log(error);
+    throw error;
   } finally {
     await client.end();
   }
